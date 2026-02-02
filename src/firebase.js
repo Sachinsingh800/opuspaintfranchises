8289760cfa3c959eba0f44e9d57404d33b29ae8d
sachinsingh800 <sachinsinghgnc@gmail.com>
2026-02-02 22:45:09 +0530

    css

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASwVIYZRz3q0bFa3_hCJr5aAuX6LqfZxY",
-  authDomain: "birlaopuspaintsfranchise-dd3e3.firebaseapp.com",
-  projectId: "birlaopuspaintsfranchise-dd3e3",
-  storageBucket: "birlaopuspaintsfranchise-dd3e3.firebasestorage.app",
-  messagingSenderId: "1025380636445",
-  appId: "1:1025380636445:web:76d7b8f47bbecd317ff3f4",
-  measurementId: "G-RCPH51DTYW",
-  databaseURL: "https://birlaopuspaintsfranchise-dd3e3-default-rtdb.firebaseio.com",
+  apiKey: "AIzaSyBofoKcZwdLknSVNMX79zslPwb6kmIVugU",
+  authDomain: "birlaopuspaintsfranchise.firebaseapp.com",
+  databaseURL: "https://birlaopuspaintsfranchise-default-rtdb.firebaseio.com",
+  projectId: "birlaopuspaintsfranchise",
+  storageBucket: "birlaopuspaintsfranchise.firebasestorage.app",
+  messagingSenderId: "249113861249",
+  appId: "1:249113861249:web:296b7a817983cb7f1bc180",
+  measurementId: "G-2MSQNDTVJT",
+  databaseURL: "https://birlaopuspaintsfranchise-default-rtdb.firebaseio.com",
 };
 
 export const app = initializeApp(firebaseConfig);
